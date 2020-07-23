fe9f71371a1d270ab69f5b8b70b1c4fe554a9022
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2020-07-23 17:56:17 +0900

    実装1終了

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = 'c2738adec694ccb7b4ea950c94cbc39dd408f4d61171fefd86f804206c319977c0c581557a634772dcbe3e43de9b0deab09814018ebc3c8ca00e4daa5ab5dfcd'
