e56bd94bbf65a0abf208fadeeff9750cd74fe81e
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2020-07-24 15:28:31 +0900

    いいね機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_133206) do
+ActiveRecord::Schema.define(version: 2020_07_24_044626) do
 
   create_table "cats", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
     t.text "body"
     t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "cat_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
