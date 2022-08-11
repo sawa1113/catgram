6681234464cc19102603e2f8d1b681290fdce9c5
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2022-08-11 22:06:34 +0900

    アプリ立ち上げ、ログイン機能実装、ユーザーのCRUD作成

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '123bb03f789b062d32ab636951ef05a08e2e08e027c01c0e7514415b1f7d20d5c6c4fb0f68b688e93ca6c9d11d2b65b41d7685fb138bff1e3a8eec6c71274116'
